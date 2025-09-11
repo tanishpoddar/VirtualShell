0f8abc43084e75e9188fa826031ef4dc09e2b54a
Tanish <tanish.iaf1852005@gmail.com>
2025-09-11 11:57:52 +0000

    changed-code-files

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,9 @@ export default function Footer() {
 
   return (
     <footer className="py-6 md:px-8 md:py-4 bg-background border-t">
-      <div className="container flex flex-col items-center justify-center gap-2 md:flex-row md:justify-between">
-        <p className="text-balance text-center text-sm leading-loose text-muted-foreground">
-          Copyright © {currentYear} | School of Computing - SRM Institute of Science & Technology | All Rights Reserved
+      <div className="container flex flex-col items-center justify-center gap-2 text-center md:flex-row md:justify-between">
+        <p className="text-balance text-sm leading-loose text-muted-foreground">
+          Copyright © {currentYear} | School of Computing - SRM Institute of Science &amp; Technology | All Rights Reserved
         </p>
       </div>
     </footer>
